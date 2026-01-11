a9826567e029f0647a9b4dc003838f41f840d16b
MASTERZSS <mastergamer74rtg@gmail.com>
2026-01-10 22:33:23 -0500

    Update SettingsLayer.cpp

--- mod/src/SettingsLayer.cpp
+++ mod/src/SettingsLayer.cpp
@@ -19,9 +19,21 @@ bool AutoSaveSettingsLayer::init() {
 
 void AutoSaveSettingsLayer::buildUI() {
     auto win = CCDirector::sharedDirector()->getWinSize();
-    auto title = CCLabelBMFont::create("Auto Save", "bigFont.fnt");
+    auto title = CCLabelBMFont::create("Auto Save Settings", "bigFont.fnt");
     title->setPosition({win.width/2, win.height - 60});
     addChild(title);
+
+    auto autoLabel = CCLabelBMFont::create("Enable Auto Save", "goldFont.fnt");
+    autoLabel->setPosition({120, win.height - 120});
+    addChild(autoLabel);
+
+    auto toggleOn = CCLabelBMFont::create("Toggle", "goldFont.fnt");
+    auto toggleItem = CCMenuItemLabel::create(toggleOn, this, menu_selector(AutoSaveSettingsLayer::onToggleAuto));
+    toggleItem->setPosition({win.width - 120, win.height - 120});
+    auto menu = CCMenu::create(toggleItem, nullptr);
+    menu->setPosition({0,0});
+    addChild(menu);
+
     auto saveNowBtn = CCMenuItemSpriteExtra::create(
         CCSprite::createWithSpriteFrameName("GJ_button_01.png"),
         this, menu_selector(AutoSaveSettingsLayer::onSaveNow)
@@ -29,30 +41,55 @@ void AutoSaveSettingsLayer::buildUI() {
     auto lbl = CCLabelBMFont::create("Save Now", "goldFont.fnt");
     lbl->setPosition({saveNowBtn->getContentSize().width/2, saveNowBtn->getContentSize().height/2});
     saveNowBtn->addChild(lbl);
-    saveNowBtn->setPosition({win.width/2, win.height - 140});
-    auto logsBtn = CCMenuItemSpriteExtra::create(
-        CCSprite::createWithSpriteFrameName("GJ_button_01.png"),
-        this, menu_selector(AutoSaveSettingsLayer::onViewLogs)
+    saveNowBtn->setPosition({win.width/2, win.height - 200});
+    auto menu2 = CCMenu::create(saveNowBtn, nullptr);
+    menu2->setPosition({0,0});
+    addChild(menu2);
+
+    auto intervalLabel = CCLabelBMFont::create("Interval (min)", "goldFont.fnt");
+    intervalLabel->setPosition({120, win.height - 160});
+    addChild(intervalLabel);
+
+    auto inc = CCMenuItemSpriteExtra::create(
+        CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"),
+        this, menu_selector(AutoSaveSettingsLayer::onIntervalChange)
     );
-    auto lbl2 = CCLabelBMFont::create("View Logs", "goldFont.fnt");
-    lbl2->setPosition({logsBtn->getContentSize().width/2, logsBtn->getContentSize().height/2});
-    logsBtn->addChild(lbl2);
-    logsBtn->setPosition({win.width/2, win.height - 200});
-    auto menu = CCMenu::create(saveNowBtn, logsBtn, nullptr);
-    menu->setPosition({0,0});
-    addChild(menu);
+    inc->setTag(1);
+    inc->setPosition({win.width - 80, win.height - 160});
+    auto dec = CCMenuItemSpriteExtra::create(
+        CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"),
+        this, menu_selector(AutoSaveSettingsLayer::onIntervalChange)
+    );
+    dec->setTag(-1);
+    dec->setPosition({win.width - 160, win.height - 160});
+    auto menu3 = CCMenu::create(inc, dec, nullptr);
+    menu3->setPosition({0,0});
+    addChild(menu3);
 }
 
-void AutoSaveSettingsLayer::onSaveNow(CCObject* sender) {
+void AutoSaveSettingsLayer::onSaveNow(CCObject*) {
     FLAlertLayer::create("Auto Save", "Trigger the game's save now? This will attempt to simulate the game's save button.", "Yes", "No",
         [](auto, auto){ bool ok = AutoSaveManager::instance().triggerManualSave(); if (ok) FLAlertLayer::create("Auto Save","Save invoked","OK")->show(); else FLAlertLayer::create("Auto Save","Could not find save control","OK")->show(); },
         nullptr)->show();
 }
 
-void AutoSaveSettingsLayer::onViewLogs(CCObject* sender) {
-    auto logs = AutoSaveManager::instance().getLogs();
-    std::string text;
-    for (auto& l : logs) { text += l + "\n"; }
-    if (text.empty()) text = "No logs";
-    FLAlertLayer::create("Auto Save Logs", text.c_str(), "OK")->show();
+void AutoSaveSettingsLayer::onToggleAuto(CCObject*) {
+    AutoSaveManager::instance().settings.autoEnabled = !AutoSaveManager::instance().settings.autoEnabled;
+    if (AutoSaveManager::instance().settings.autoEnabled) {
+        AutoSaveManager::instance().scheduleNext();
+        FLAlertLayer::create("Auto Save", "Auto-trigger enabled. Protections active.", "OK")->show();
+    } else {
+        FLAlertLayer::create("Auto Save", "Auto-trigger disabled.", "OK")->show();
+    }
+}
+
+void AutoSaveSettingsLayer::onIntervalChange(CCObject* sender) {
+    auto item = static_cast<CCNode*>(sender);
+    int delta = item->getTag();
+    int cur = AutoSaveManager::instance().settings.intervalMinutes;
+    cur += delta;
+    if (cur < 10) cur = 10;
+    if (cur > 60) cur = 60;
+    AutoSaveManager::instance().settings.intervalMinutes = cur;
+    FLAlertLayer::create("Auto Save", ("Interval set to " + std::to_string(cur) + " minutes").c_str(), "OK")->show();
 }
